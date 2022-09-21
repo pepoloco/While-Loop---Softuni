61486a046a8159e5e4b3cb9610bf05f85b8d5a21
Pepo Banchev <56348332+pepoloco@users.noreply.github.com>
2022-09-21 20:10:33 +0300

    Add files via upload

---
+++ oldBooks.js
@@ -0,0 +1,39 @@
+function oldLibraby(input) {
+
+    let favBook = input[0];
+    
+    let index = 1;
+    let newBook = input[index];
+    
+    
+    let booksIsFound = false;
+    
+    let checkedBooks = 0; // counter za kolko knigi sme proverili.
+    
+    while (newBook !== "No More Books"){
+        checkedBooks++;
+        if (newBook === favBook){
+            booksIsFound = true;
+            break;
+        }else {
+            booksIsFound = false;
+        }
+        index++;
+        newBook = input[index];
+        
+    }
+        if ( booksIsFound === true){
+            console.log(`You checked ${checkedBooks - 1} books and found it.`)
+        }else{
+            console.log(`The book you search is not here!`);
+            console.log(`You checked ${checkedBooks} books.`);
+        }
+    
+    
+    }
+    oldLibraby(["The Spot",
+    "Hunger Games",
+    "Harry Potter",
+    "Torronto",
+    "Spotify",
+    "No More Books"]);
